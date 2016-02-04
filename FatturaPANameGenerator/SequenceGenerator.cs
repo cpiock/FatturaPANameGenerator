04aee5453b05620d6278dab95613099bf9624b09
cpiock <cpi@nbk-cpi.systems.local>
2016-02-04 14:16:12 +0100

    inital commit

---
+++ FatturaPANameGenerator/SequenceGenerator.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatturaPANameGenerator
+{
+    public class SequenceGenerator
+    {        
+        char[] numberSequence = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        char[] charSequence = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+        public string NextCode(string value)
+        {
+            char[] chars = value.ToCharArray();
+            for (int i = chars.Length - 1; i >= 0; i--)
+            {
+                int index = Array.IndexOf(numberSequence, chars[i]);
+                //if not last int
+                if (index >= 0)
+                {
+                    if (index < numberSequence.Length-1)
+                    {
+                        //get next valid int
+                        chars[i] = numberSequence[index + 1];
+                        break;
+                    }
+                    //if maximum int
+                    else
+                    {
+                        //if not the first char in given string
+                        if (i > 0)
+                        {
+                            index = Array.IndexOf(numberSequence, chars[i - 1]);
+                            if (index <= numberSequence.Length - 1)
+                            {
+                                //set first int
+                                chars[i] = numberSequence[0];
+                            }
+                        }
+                        else
+                        {      
+                            //check if it is a char                      
+                            int charIndex = Array.IndexOf(charSequence, chars[i]);
+                            if (charIndex >= 0)
+                            {
+                                //is the next char not the last char
+                                if (charIndex < charSequence.Length - 1)
+                                {
+                                    chars[i] = charSequence[charIndex + 1];
+                                    break;
+                                }
+                                else
+                                {
+                                    //is the next char the last char then restart by the first
+                                    index = Array.IndexOf(charSequence, chars[i - 1]);
+                                    if (index < charSequence.Length - 1)
+                                    {
+                                        chars[i] = charSequence[0];
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                //if not take the first char
+                                chars[i] = charSequence[0];
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    int charIndex = Array.IndexOf(charSequence, chars[i]);
+                    if (charIndex >= 0)
+                    {
+                        //if it's not the last char
+                        if (charIndex < charSequence.Length-1)
+                        {
+                            //get the next char
+                            chars[i] = charSequence[charIndex + 1];
+                            break;
+                        }
+                        else
+                        {
+                            //if it is not the first char in string
+                            if (i > 0)
+                            {
+                                index = Array.IndexOf(charSequence, chars[i - 1]);
+                                if (index < charSequence.Length - 1)
+                                {
+                                    //set the first char
+                                    chars[i] = charSequence[0];
+                                }
+                                else
+                                {
+                                    //set the first char in the next position of the string
+                                    chars[i + 1] = charSequence[0];
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                //set the first char in the next position of the string
+                                chars[i+1] = charSequence[0];
+                            }
+                        }
+                    }
+
+                }
+            }
+            return new string(chars);
+        }
+    }
+}
---
+++ FatturaPANameGeneratorTests/SequenceGeneratorTests.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FatturaPANameGenerator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatturaPANameGenerator.Tests
+{
+    [TestClass()]
+    public class SequenceGeneratorTests
+    {
+        SequenceGenerator generator;
+       [TestInitialize]
+        public void SetUp()
+        {
+            generator = new SequenceGenerator();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            generator = null;
+        }
+
+        [TestMethod()]
+        public void NextCodeTestA00001()
+        {
+            string oldCode = "A00001";
+            
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "A00002");
+        }
+
+        [TestMethod()]
+        public void NextCodeTest000001()
+        {
+            string oldCode = "000001";
+
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "000002");
+        }
+        [TestMethod()]
+        public void NextCodeTest999999()
+        {
+            string oldCode = "999999";
+
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "A00000");
+        }
+
+        [TestMethod()]
+        public void NextCodeTestA00009()
+        {
+            string oldCode = "A00009";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "A00010");
+        }
+        [TestMethod()]
+        public void NextCodeTestA12999()
+        {
+            string oldCode = "A12999";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "A13000");
+        }
+        [TestMethod()]
+        public void NextCodeTestA19999()
+        {
+            string oldCode = "A19999";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "A20000");
+        }
+        [TestMethod()]
+        public void NextCodeTestA99999()
+        {
+            string oldCode = "A99999";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "B00000");
+        }
+
+        [TestMethod()]
+        public void NextCodeTestZZ9999()
+        {
+            string oldCode = "ZZ9999";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "ZZA000");
+        }
+
+        [TestMethod()]
+        public void NextCodeTestZ99999()
+        {
+            string oldCode = "Z99999";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "ZA0000");
+        }
+        [TestMethod()]
+        public void NextCodeTestZ99123()
+        {
+            string oldCode = "Z99123";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "Z99124");
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void NextCodeTestZZZZZZ()
+        {
+            string oldCode = "ZZZZZZ";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+           
+        }
+        [TestMethod()]
+        public void NextCodeTestZBR234()
+        {
+            string oldCode = "ZBR234";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "ZBR235");
+        }
+        [TestMethod()]
+        public void NextCodeTestZBR934()
+        {
+            string oldCode = "ZBR934";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "ZBR935");
+        }
+        [TestMethod()]
+        public void NextCodeTestZBR999()
+        {
+            string oldCode = "ZBR999";
+            SequenceGenerator generator = new SequenceGenerator();
+
+            string newCode = generator.NextCode(oldCode);
+
+            Assert.AreEqual(newCode, "ZBS000");
+        }
+    }
+}
